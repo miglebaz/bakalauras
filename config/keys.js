c79fd2128eaee6f409cd2c85522aca5bbd576c7e
Migle Bazeviciute <mbazeviciute@kayak.com>
2019-05-05 20:03:26 +0300

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://bakalauras:bakis@devconection-shard-00-00-5llwq.mongodb.net:27017,devconection-shard-00-01-5llwq.mongodb.net:27017,devconection-shard-00-02-5llwq.mongodb.net:27017/test?ssl=true&replicaSet=devconection-shard-0&authSource=admin&retryWrites=true",
+  secretOrKey: "secret"
+};
